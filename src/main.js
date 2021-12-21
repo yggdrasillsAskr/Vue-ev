def9547ae473b5d9b416ac82d62b249d9a699343
Yggdrasill <S18171275776@outlook.com>
2021-12-21 17:07:08 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+//引入全局样式
+import './assets/global.less'
+
+//引入axios
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://www.liulongbin.top:3008'
 
 Vue.config.productionTip = false
 
